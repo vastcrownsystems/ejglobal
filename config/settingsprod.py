4811f8734f4af13f2b386eda4eb7872c8360a523
Michael Aroworade <ayoaro85@gmail.com>
2026-02-25 12:03:06 +0100

    host added

--- config/settingsprod.py
+++ config/settingsprod.py
@@ -37,7 +37,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['http://139.59.185.33/']
 
 
 # Application definition
