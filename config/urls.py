3d3f92f4bd160f960fd43fffa17c961226d36056
Michael Aroworade <ayoaro85@gmail.com>
2026-02-25 18:08:36 +0100

    few updates

--- config/urls.py
+++ config/urls.py
@@ -23,7 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('apps.core.urls')),
     path('accounts/', include('apps.accounts.urls')),
-    path("accounts/", include("django.contrib.auth.urls")),
+    # path("accounts/", include("django.contrib.auth.urls")),
     path('sales/', include('apps.sales.urls')),
     path('catalog/', include('apps.catalog.urls')),
     path('orders', include("apps.orders.urls")),
